f00c50f3eaba1bb4ad2c51c552851ab4188b6c3b
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-06 18:52:49 +0100

    Initial commit

---
+++ CamelCase.js
@@ -0,0 +1,11 @@
+function toCamelCase(str){
+
+    let newArray = str.split(/-|_/)
+
+    return newArray.shift() //take first item out of newArray
+      + newArray.map(item => (item.charAt(0) //loop over array and access the first letter
+                              .toUpperCase() //turn the letter into upper case
+                              + item.slice(1))) //add the rest of the word to the upper case letter but WITHOUT the first original letter
+      .join('') //turns all items to Capitalized
+
+}
