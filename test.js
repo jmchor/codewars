ba347594cf7f5e1b4ba88ca28fe47a008ebc4eb4
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-07 20:37:30 +0100

    Add several katas

---
+++ test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+function palindrome(str) {
+        const regex = /[^A-Za-z0-9]/g;
+
+        str = str.toLowerCase().replace(regex, '');
+
+        for (let i = 0; i < str.length / 2; i++) {
+                if (str[i] !== str[str.length - 1 - i]) {
+                        console.log(false);
+                }
+        }
+        console.log(true);
+        return true;
+}
+palindrome('A man, a plan, a canal, Panama!');
