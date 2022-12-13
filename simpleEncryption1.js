0f25e2b7135ad713dee9a0f5de959074f5732607
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-13 14:28:18 +0100

    Solved

--- simpleEncryption1.js
+++ simpleEncryption1.js
@@ -4,48 +4,59 @@ function encrypt(text, n) {
         if (text === '' || n <= 0) return text;
         if (text === null) return null;
 
-        let encryptArray = text.split('');
+        let textArray = text.split('');
         let odd = [];
         let even = [];
-        let encryption = [];
+        let joinedArray = [];
+
+        /*
+
+        Could also be written:
+
+        odd = textArray.filter((item, ctrl) => ctrl % 2 != 0)
+        even = textArray.filter((item, ctrl) => ctrl % 2 === 0)
+
+        */
 
         for (let i = 1; i <= n; i++) {
-                for (let j = 0; j < encryptArray.length; j++) {
+                for (let j = 0; j < textArray.length; j++) {
                         if (j % 2 != 0) {
-                                odd.push(encryptArray[j]);
+                                odd.push(textArray[j]);
                         } else if (j % 2 === 0) {
-                                even.push(encryptArray[j]);
+                                even.push(textArray[j]);
                         }
                 }
 
-                encryption = odd.concat(even).join('');
-                encryptArray = encryption.split('');
+                joinedArray = odd.concat(even).join('');
+                textArray = joinedArray.split('');
                 odd = [];
                 even = [];
         }
-        return encryption;
+        return joinedArray;
 }
 
 function decrypt(encryptedText, n) {
         if (encryptedText === '' || n <= 0) return encryptedText;
         if (encryptedText === null) return null;
-
-        let decryptArray = encryptedText.split('')
-        let middle = decryptArray.length / 2
-        let firstHalf = decryptArray.slice(middle)
-        let secondHalf = decryptArray.slice(0,middle)
-        let decryption = []
+        let decryptArray = encryptedText.split('');
+        let middle = decryptArray.length / 2;
+        let first = decryptArray.slice(middle);
+        let second = decryptArray.slice(0, middle);
+        let decryption = [];
         let decryptedText;
 
         for (let i = 1; i <= n; i++) {
                 for (let j = 0; j < decryptArray.length; j++) {
-
-                        decryption.push(firstHalf[j])
-                        decryption.push(secondHalf[j])
-
+                        decryption.push(first[j]);
+                        decryption.push(second[j]);
                 }
 
-              decryptedText = decryption.join('')
+                decryptedText = decryption.join('');
+                decryptArray = decryptedText.split('');
+                decryption = [];
+                middle = decryptArray.length / 2;
+                first = decryptArray.slice(middle);
+                second = decryptArray.slice(0, middle);
         }
-
+        return decryptedText;
 }
