55c06c958a5ebb9d64ffb1393e09357b5df55566
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-18 22:53:45 +0100

    Solved Day 2 (ugly tho)

--- AoC/day2.js
+++ AoC/day2.js
@@ -2,17 +2,43 @@
 const input = require('fs').readFileSync('./d2_input.txt', 'utf-8').split('\n');
 
 const inputArray = input.map((data) => data.split(' ').map((element) => element));
-const numArray = inputArray.map(
-        (elem) =>
-                elem.map((item) => {
-                        if (item === 'A' || item === 'X') return 1;
-                        if (item === 'B' || item === 'Y') return 2;
-                        if (item === 'C' || item === 'Z') return 3;
-                })
-
-        // loop over array, if array[i+1] > array[i] add  array[i+1] to sum plus 6
-        // if array[i+1] < array[i] add 1 to sum
-        // if array[i+1] === array[i] add array[i+1] to sum plus 3
+const numArray = inputArray.map((elem) =>
+        elem.map((item) => {
+                if (item === 'A' || item === 'X') return 1;
+                if (item === 'B' || item === 'Y') return 2;
+                if (item === 'C' || item === 'Z') return 3;
+        })
 );
 
-console.log(numArray);
+const sumArray = numArray.map((elem) => {
+        for (let i = 0; i < elem.length; i++) {
+                // you win
+
+                if (elem[i + 1] === 3 && elem[i] === 2) return 9;
+                if (elem[i + 1] === 3 && elem[i] === 1) return 8;
+                if (elem[i + 1] === 3 && elem[i] === 3) return 7;
+
+                if (elem[i + 1] === 1 && elem[i] === 2) return 1;
+                if (elem[i + 1] === 1 && elem[i] === 1) return 3;
+                if (elem[i + 1] === 1 && elem[i] === 3) return 2;
+
+                if (elem[i + 1] === 2 && elem[i] === 1) return elem[i] + 3;
+                if (elem[i + 1] === 2 && elem[i] === 2) return elem[i] + 3;
+                if (elem[i + 1] === 2 && elem[i] === 3) return elem[i] + 3;
+        }
+});
+
+console.log(sumArray.reduce((a, b) => a + b, 0));
+
+/*
+
+
+
+if (elem[i + 1] === 1 && elem[i] === 3 || elem[i + 1] === 1 && elem[i] === 2 || elem[i + 1] === 1 && elem[i] === 1) {return 2 || 1 || 3}
+else if (elem[i + 1] === 2 && elem[i] === 3 || elem[i + 1] === 2 && elem[i] === 2 || elem[i + 1] === 2 && elem[i] === 1) {return elem[i] + 3}
+else if (elem[i + 1] === 3 && elem[i] === 3 || elem[i + 1] === 3 && elem[i] === 2 || elem[i + 1] === 3 && elem[i] === 1) {return 1 || 3 || 2}
+
+
+
+
+*/
