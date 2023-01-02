32cc76b0bb8c4a492bfe858c1f0c28fafded3972
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-02 19:29:06 +0100

    Solve condition for string beginning with #

--- hashtaggenerator.js
+++ hashtaggenerator.js
@@ -1,15 +1,21 @@
 function generateHashtag(str) {
         let stringArray = str.trim().split(' ');
-        // or stringArray = str.split().filter(item => item !== '');
+        if (stringArray[0][0] === '#') {
+                const replacement = stringArray[0].replace('#', '');
+                stringArray.splice(0, 1, replacement);
+        }
+
         for (let i = 0; i < stringArray.length; i++) {
                 stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
         }
+
         stringArray.unshift('#');
         stringArray = stringArray.join('');
+
         if (stringArray.length > 140) return false;
-        if (!str.length || !stringArray.length) return false;
+        if (!str.length || str === ' ' || !stringArray.length) return false;
 
         console.log(stringArray);
 }
 
-console.log(generateHashtag(' Hello there thanks                        for trying my Kata'));
+console.log(generateHashtag(''));
