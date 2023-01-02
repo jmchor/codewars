06426a93e51b60e600146da67d1decb58de6824f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-02 18:49:41 +0100

    Fuse words together

--- hashtaggenerator.js
+++ hashtaggenerator.js
@@ -1,9 +1,10 @@
 function generateHashtag(str) {
-        const stringArray = str.split(' ');
+        let stringArray = str.split(' ');
         for (let i = 0; i < stringArray.length; i++) {
                 stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
         }
         stringArray.unshift('#');
+        stringArray = stringArray.join('');
 
         console.log(stringArray);
 }
