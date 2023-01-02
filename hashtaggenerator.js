7762068f01c8863ed78257facc4ec1d5fa784538
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-02 18:48:55 +0100

    remove spaces

---
+++ hashtaggenerator.js
@@ -0,0 +1,7 @@
+function generateHashtag(str) {
+        const stringArray = str.split(' ');
+
+        console.log(stringArray);
+}
+
+console.log(generateHashtag('Hello there thanks for trying my Kata'));
